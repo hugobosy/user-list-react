1fd763a3ef98c7a612efc6c3924958fa91342fee
HugoBosy <hkliszcz@gmail.com>
2022-06-02 15:30:49 +0200

    add user avatar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import {users} from "./data/data";
 import {UsersList} from "./components/UsersList";
 
 function App() {
+const user = [...users];
+const userSort = user.sort((a,b)=> {
+    if(a.username < b.username) {
+        return -1
+    }
+
+    if(a.username > b.username) {
+        return 1
+    }
+
+    return 0
+});
 
   return (
     <div className="App">
-      <UsersList users={users}/>
+      <UsersList users={userSort}/>
     </div>
   );
 }
--- src/components/UserListOneItem.js
+++ src/components/UserListOneItem.js
@@ -4,8 +4,11 @@ export const UserListOneItem = props => (
     <li key={props.user.username}>
         <div className="user">
             <div>
+                <img src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`} alt="Avatar"/>
+
                 <strong>{props.user.first_name} {props.user.last_name} </strong>
-                <em>Płeć {props.user.gender}</em>
+                <em>Płeć {props.user.gender} </em>
+                <span><strong>Login: </strong> {props.user.username}</span>
             </div>
         </div>
     </li>
